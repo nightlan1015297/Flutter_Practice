16182ee75b573210ce90d87332a65fb0a7750a88
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-10 22:27:03 +0800

    example

---
+++ flutter_bloc_practice/lib/cubit/counter_state.dart
@@ -0,0 +1,15 @@
+part of 'counter_cubit.dart';
+
+/* Create new State class to storage the state information*/
+
+class CounterState {
+  
+  /*Declare variable storage state info*/
+  int counterValue;
+  
+  /* Initialization constructor*/
+  CounterState({
+    @required this.counterValue,
+  });
+  
+}
---
+++ flutter_bloc_practice/lib/main.dart
@@ -0,0 +1,102 @@
+/********************************************************/
+/*                 Flutter example                      */ 
+/********************************************************/
+/* Following code shows the example of using Bloc pattern
+*  in Flutter to seperate Business logic and update 
+*  User Interface. 
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /* Wrap the Material app widget with Bloc provider 
+     * This makes BlocProvider the root widget of whole APP
+     * makes the children widgets able to listen to stream 
+    */
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            /* When receiving new State from the stream  
+            *  Bloc Builder rebuild the widget then update User Interface
+            */
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                    onPressed: () {
+                      /*Push new State to the stream when state change */
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                      /*also works with 
+                      context.bloc<CounterCubit>().decrement()
+                      */
+                    },
+                    tooltip: "Decrement",
+                    child: Icon(Icons.remove)),
+
+                FloatingActionButton(
+                    onPressed: () {
+                      /*Push new State to the stream when state change */
+                      BlocProvider.of<CounterCubit>(context).increment();
+                      /*also works with 
+                      context.bloc<CounterCubit>().increment()
+                      */
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
