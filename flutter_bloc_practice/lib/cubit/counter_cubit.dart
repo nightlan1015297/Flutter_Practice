09bd0ceb93b6c9917f7f728a4dfecb7f5bcbefb1
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-10 22:32:53 +0800

    add comment

--- flutter_bloc_practice/lib/cubit/counter_cubit.dart
+++ flutter_bloc_practice/lib/cubit/counter_cubit.dart
@@ -5,7 +5,7 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue:0));
-
+  /*Use emit() to push new state to stream*/ 
   void increment() => emit(CounterState(counterValue: state.counterValue +1));
 
   void decrement() => emit(CounterState(counterValue: state.counterValue -1));
